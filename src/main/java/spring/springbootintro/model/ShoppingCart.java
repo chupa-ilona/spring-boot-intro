68b76d27ba25dc19bebfc665b06fd72dbdd5c694
IlonaChupa <ilonachupa@gmail.com>
2026-05-29 04:52:18 +0300

    fixed

--- src/main/java/spring/springbootintro/model/ShoppingCart.java
+++ src/main/java/spring/springbootintro/model/ShoppingCart.java
@@ -1,7 +1,14 @@
 package spring.springbootintro.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import java.util.Set;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
